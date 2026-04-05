777f86d0a463ef70d05571fa6a1efaa9735f2770
hmlm90020798-alt <hmlm.90020798@gmail.com>
2026-04-05 13:50:54 +0100

    Delete engine.js

--- engine.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', () => {
-    // Esconder o loader com elegância
-    setTimeout(() => {
-        document.getElementById('loader').style.opacity = '0';
-        setTimeout(() => document.getElementById('loader').remove(), 800);
-    }, 1500);
-});
